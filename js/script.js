de49a1bc326b5af43c585844206690406a4ed205
sashapoliti <sas.politi@gmail.com>
2024-03-22 14:41:46 +0100

    SNACK 2-1 - complete

--- js/script.js
+++ js/script.js
@@ -21,3 +21,56 @@ const stampGuests = guests.map((e, i) => {
 })
 
 console.log(stampGuests);
+
+/* 
+SNACK 2
+Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+1. Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo
+ES (Marco della Rovere => MARCO DELLA ROVERE);
+2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+*/
+
+const students = [
+    {
+        id : '213',
+        name : 'Marco della Rovere',
+        grades: '78'
+    },
+    {
+        id : '110',
+        name : 'Paola Cortellessa',
+        grades: '96'
+    },
+    {
+        id : '250',
+        name : 'Andrea Mantegna',
+        grades: '48'
+    },
+    {
+        id : '145',
+        name : 'Gaia Borromini',
+        grades: '74'
+    },
+    {
+        id : '196',
+        name : 'Luigi Grimaldello',
+        grades: '68'
+    },
+    {
+        id : '102',
+        name : 'Piero della Francesca',
+        grades: '50'
+    },
+    {
+        id : '120',
+        name : 'Francesca da Polenta',
+        grades: '84'
+    }
+]
+
+const upperCaseName = students.map((e) => {
+    return e.name.toUpperCase()
+})
+
+console.log(upperCaseName);
